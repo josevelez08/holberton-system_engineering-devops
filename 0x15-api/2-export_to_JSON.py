e76d87ea90b89c573511f4cde452381cf78332fd
josevelez08 <jose-0919@hotmail.com>
2021-05-03 19:20:07 -0500

    export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+import csv
+import requests
+from sys import argv
+import json
+
+
+def API_request(argv):
+    """Gather data from an API and past it to csv"""
+    todo = "https://jsonplaceholder.typicode.com/todos/?userId=" + str(argv[1])
+    users = "https://jsonplaceholder.typicode.com/users/" + str(argv[1])
+
+    responset = requests.get(todo)
+    responseu = requests.get(users)
+    json_t = responset.json()
+    json_u = responseu.json()
+    user_name = json_u.get("username")
+
+    dic_complete = {}
+    list_json = []
+    for i in json_t:
+        my_dic = {'task': '', 'completed': None, 'username': user_name}
+        task_title = i.get('title')
+        true_false = i.get('completed')
+        my_dic.update(task=task_title, completed=true_false)
+        list_json.append(my_dic)
+    dic_complete.update({argv[1]: list_json})
+
+    with open(argv[1]+"."+"csv", 'w') as file:
+        json.dump(dic_complete, file)
+
+
+if __name__ == "__main__":
+    API_request(argv)
